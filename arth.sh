d01260c46c33f6a308624bc75a0ec6bcb2a91e74
sick3356 <myselfdipu259@gmail.com>
2021-08-23 12:17:27 +0530

    dictionary

--- arth.sh
+++ arth.sh
@@ -1,8 +1,28 @@
 #! /bin/bash
 
+declare -A arthimaticOp
+
 read -p "Enter a value: " a
 read -p "Enter a value: " b
 read -p "Enter a value: " c
 
-((z=a%b,x=z+c));
+echo $((x = a + b * c))
 echo $x
+
+echo $((y = a * b + c))
+echo $y
+
+echo $((z = c + a / b))
+echo $y
+
+echo $((u = a % b + c))
+echo $u
+
+
+#STORE DICTIONARY
+arthimaticOp[x]=$x
+arthimaticOp[y]=$y
+arthimaticOp[z]=$z
+arthimaticOp[u]=$u
+
+
