9a799a0529bcc7778ee19c0afff0852146a49357
sick3356 <myselfdipu259@gmail.com>
2021-08-23 12:42:04 +0530

    array

--- arth.sh
+++ arth.sh
@@ -1,11 +1,14 @@
 #! /bin/bash
 
+#DECLARE A DICTIONARY
 declare -A arthimaticOp
 
+#USER INPUT
 read -p "Enter a value: " a
 read -p "Enter a value: " b
 read -p "Enter a value: " c
 
+#COMPUTE ARTHIMATIC OPERATION
 echo $((x = a + b * c))
 echo $x
 
@@ -20,9 +23,19 @@ echo $u
 
 
 #STORE DICTIONARY
+
 arthimaticOp[x]=$x
 arthimaticOp[y]=$y
 arthimaticOp[z]=$z
 arthimaticOp[u]=$u
 
+#TO READ THE DICTIONARY VALUE IN ARRAY
 
+for((index=0; index<=${#arithmaticOperation[@]}; index++))
+do
+  array[index]=${arithmaticOperation[result$((index+1))]}
+done
+
+#TO PRINT
+
+echo "${array[@]}"
