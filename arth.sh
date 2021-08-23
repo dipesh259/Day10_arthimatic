c0fe3c7ea033bb537257b747cccad70e859ec229
sick3356 <myselfdipu259@gmail.com>
2021-08-23 11:25:29 +0530

    compute

--- arth.sh
+++ arth.sh
@@ -1,11 +1,8 @@
 #! /bin/bash
 
-#USER INPUT
 read -p "Enter a value: " a
 read -p "Enter a value: " b
 read -p "Enter a value: " c
 
-echo "Value of a is " $a
-echo "Value of b is " $b
-echo "Value of c is " $c
-
+((z=a+b,x=z*c));
+echo $x
