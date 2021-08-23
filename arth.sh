5b01c5f59af783a5677d128e471184fb73a4ce23
sick3356 <myselfdipu259@gmail.com>
2021-08-23 11:08:56 +0530

    first commmit

---
+++ arth.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+#USER INPUT
+read -p "Enter a value: " a
+read -p "Enter a value: " b
+read -p "Enter a value: " c
+
+echo "Value of a is " $a
+echo "Value of b is " $b
+echo "Value of c is " $c
+
